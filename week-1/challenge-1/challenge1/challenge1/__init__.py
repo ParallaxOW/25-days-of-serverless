465192faa0ca8a959596b4db9a471544576c15b4
Russ <parallax.hots@gmail.com>
2019-12-02 12:09:58 -0500

    clean up

--- week-1/challenge-1/challenge1/challenge1/__init__.py
+++ week-1/challenge-1/challenge1/challenge1/__init__.py
@@ -18,18 +18,8 @@ container_name = os.getenv("ContainerName")
 def main(req: func.HttpRequest) -> func.HttpResponse:
 	logging.info('Python HTTP trigger function processed a request.')
 	blob_name = determine_file()
-	#token = get_sas_token(container_name, blob_name)
-	#blob_url = build_blob_url(account_name, container_name, blob_name, token)
 	image = get_image(blob_name)
 	return func.HttpResponse(image, mimetype="image/png")
-
-def get_sas_token(containerName, blobName):
-	generator = BlobSharedAccessSignature(account_name, account_key)
-	return generator.generate_blob(container_name=containerName, blob_name=blobName, permission=ContainerPermissions.READ,expiry=datetime.utcnow() + timedelta(hours=1), start=datetime.utcnow())
-
-def build_blob_url(accountName, containerName, blobName, token):
-	return ("https://{}.blob.core.windows.net/{}/{}?{}").format(accountName,containerName,blobName,token)
-
 
 def determine_file():
 	sequence = ["nun.png","gimmel.png","hay.png","shin.png"]
